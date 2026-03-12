d5999259e5269320e14e4791ebfb393c5be8206f
jakir-md <mdjakir.cse.ju@gmail.com>
2026-03-12 17:27:16 +0600

    user controller and services and modules added

---
+++ backend/src/modules/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma } from 'generated/prisma/client';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() userData: Prisma.UserCreateInput) {
+    return this.usersService.create(userData);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
+}
